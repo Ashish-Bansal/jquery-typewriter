96fac677fb14471dbd67ed60050ca31368f47779
Ashish Bansal <bansal.ashish096@gmail.com>
2015-11-14 02:27:47 +0530

    Created demo website

---
+++ main.js
@@ -0,0 +1,10 @@
+$('#typewriter').typewriter({
+	prefix : "# ",
+	text : ["Hey, this looks cool, right ?", "This keeps looping again and again.", "Wanna use this cool plugin ?", "See below given usage instructions."],
+	typeDelay : 50,
+	waitingTime : 1500,
+	blinkSpeed : 200
+});
+
+// Load Code Highlighter
+hljs.initHighlightingOnLoad();
